5382fb2a6d4e937af78f3702ac48cf2b295b347c
JayramNanoskar <jayram.n978@gmail.com>
2021-11-17 21:14:11 +0530

    Building the services API & Configuring server port

---
+++ movie-catalog-service/src/main/java/com/jayram/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,24 @@
+package com.jayram.moviecatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jayram.moviecatalogservice.models.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+		
+		return Collections.singletonList(
+				new CatalogItem("Krish", "Test", 4)
+				);
+		
+	}
+}

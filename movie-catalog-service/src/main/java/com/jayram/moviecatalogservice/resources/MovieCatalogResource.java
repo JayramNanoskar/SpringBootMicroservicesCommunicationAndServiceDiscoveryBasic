ddd76f754f0dd45c0765e85d7f48f572eacf4504
JayramNanoskar <jayram.n978@gmail.com>
2021-11-18 20:03:38 +0530

    Using a Bean to create RestTemplate instance

--- movie-catalog-service/src/main/java/com/jayram/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/jayram/moviecatalogservice/resources/MovieCatalogResource.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,11 @@ import com.jayram.moviecatalogservice.models.Rating;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-		
-		RestTemplate restTemplate = new RestTemplate();
 		
 		List<Rating> ratings = Arrays.asList(
 				new Rating("1234", 4),

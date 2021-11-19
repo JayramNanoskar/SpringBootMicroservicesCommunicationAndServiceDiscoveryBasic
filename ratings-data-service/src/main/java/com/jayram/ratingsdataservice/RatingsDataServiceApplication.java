de47263cf6bb71b99e866c7d80b4333b012278d4
JayramNanoskar <jayram.n978@gmail.com>
2021-11-20 01:21:53 +0530

    Creating a Eureka clients

--- ratings-data-service/src/main/java/com/jayram/ratingsdataservice/RatingsDataServiceApplication.java
+++ ratings-data-service/src/main/java/com/jayram/ratingsdataservice/RatingsDataServiceApplication.java
@@ -2,8 +2,10 @@ package com.jayram.ratingsdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //Optional
 public class RatingsDataServiceApplication {
 
 	public static void main(String[] args) {
